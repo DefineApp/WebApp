55b785f37cd31edd64b5f58885bdd4d13f08c228
Syndicate555 <saziz028@uottawa.ca>
2020-04-25 03:07:49 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Login from "./Components/Login";
+import Home from "./Components/pages/Home";
+import About from "./Components/pages/About";
+import Navbar from "./Components/layout/Navbar";
+
+const App = () => {
+  return (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
+  );
+};
+
+export default App;
